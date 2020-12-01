304fa61cee47bcdbc6ef0526954878d73f9ae320
Sarah Maris <sarah.maris@udacity.com>
2020-12-01 00:04:05 -0500

    Add modal to POST new team member

--- src/TeamList.js
+++ src/TeamList.js
@@ -4,7 +4,53 @@ import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 const TeamList = (props) => {
   const [teamList, setTeamList] = useState(null);
-  const [showModal, setShowModal] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+  const [showWarning, setShowWarning] = useState(false);
+  const [teamMember, setTeamMember] = useState({
+    name: '',
+    department: '',
+    id: null,
+  });
+
+  const addTeamMember = async (e) => {
+    e.preventDefault();
+    if (!teamMember.name || !teamMember.department) {
+      setShowWarning(true);
+      return;
+    }
+    try {
+      const response = await fetch(
+        process.env.REACT_APP_DATABASE_URL + '/team',
+        {
+          method: 'POST',
+          headers: {
+            Authorization: `Bearer ${props.token}`,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(teamMember),
+        }
+      );
+      const data = await response.json();
+
+      // Update teamList and reset teamMember when add succeeds
+      if (data.success) {
+        teamMember.id = data.team_member.id;
+        setTeamList(teamList.concat(teamMember));
+        setTeamMember({
+          name: '',
+          department: '',
+          id: null,
+        });
+        setShowModal(false);
+      }
+    } catch (e) {
+      console.error('oops!', e);
+    }
+  };
+  const editTeamMember = (id) => {
+    openModal();
+    console.log(id);
+  };
 
   const deleteTeamMember = async (id) => {
     try {
@@ -73,7 +119,10 @@ const TeamList = (props) => {
                 <span className='department'>{member.department}</span>
               </div>
               <div className='admin'>
-                <button className='edit'>
+                <button
+                  className='edit'
+                  onClick={(e) => editTeamMember(member.id, e)}
+                >
                   <FontAwesomeIcon icon={faEdit} />
                 </button>
                 <button
@@ -92,7 +141,43 @@ const TeamList = (props) => {
       </button>
       <div className={showModal ? 'open modal' : 'closed model'}>
         <section className='teamModal'>
-          <button onClick={closeModal}>X</button>
+          <h3>Add Team Member</h3>
+          <form className='teamForm' onSubmit={addTeamMember}>
+            <label htmlFor='nameInput'>Name</label>
+            <input
+              id='nameInput'
+              className='teamInput'
+              type='text'
+              name='memberName'
+              onChange={(e) =>
+                setTeamMember((state) => ({ ...state, name: e.target.value }))
+              }
+              value={teamMember.name}
+            />
+
+            <label htmlFor='deptInput'>Department</label>
+            <input
+              id='deptInput'
+              className='teamInput'
+              type='text'
+              name='memberDepartment'
+              onChange={(e) =>
+                setTeamMember((state) => ({
+                  ...state,
+                  department: e.target.value,
+                }))
+              }
+              value={teamMember.department}
+            />
+            <p className={showWarning ? 'warning' : 'closed'}>
+              Please enter all data
+            </p>
+            <input type='submit' value='Submit' className='submitButton' />
+          </form>
+
+          <button className='closeButton' onClick={closeModal}>
+            X
+          </button>
         </section>
       </div>
     </section>

044c60e09ac8fcf87a996c261ef6feb2ca5d6fd1
Sarah Maris <sarah.maris@udacity.com>
2020-12-12 14:53:22 -0500

    Style welcome page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 import Authorization from './Authorization';
 import loading from './images/blocks-loader.gif';
+import logo from './images/projx-logo.svg';
 
 function App() {
   const {
@@ -35,7 +36,18 @@ function App() {
       </div>
     );
   } else {
-    return <button onClick={loginWithRedirect}>Log in</button>;
+    return (
+      <div className='welcome'>
+        <div className='welcome-left'>
+          <img alt='ProjX logo' src={logo} />
+        </div>
+        <div className='welcome-right'>
+          <h1 className='headline'>Welcome to ProjX</h1>
+          <p className='subhead'>Project Staffing made easy!</p>
+          <button onClick={loginWithRedirect}>Log in</button>
+        </div>
+      </div>
+    );
   }
 }
 
